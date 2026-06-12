aa1489bd16051cae73895b9834e3ad529087709d
vishakha bhardwaj <103990579+Vishakha147bhardwaj@users.noreply.github.com>
2026-06-12 16:43:11 +0000

    comparison operators

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -10,6 +10,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// ******************** Operations ********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+const str1 = "Hello";
+const str2 = "Vishakha";
+const result = str1 + str2;
+// console.log(result);
+//console.log(+true);
+//console.log(+"");
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
 

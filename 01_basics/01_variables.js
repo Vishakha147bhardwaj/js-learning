9ae497b0bee95bda5619aed15037ad7ff4d2f304
vishakha bhardwaj <103990579+Vishakha147bhardwaj@users.noreply.github.com>
2026-06-12 12:53:08 +0000

    variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1234;
+let accountEmail = "vishkahabhardwaj147@gmail.com"
+var accountPassword ="12345"
+accountCity = "Noida"
+let accountState;
+// accountId = 2
+// console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";
+// alert(3+3) // we are using nodejs, not browser
+// Data Types in JS:
+//  number => 2^53
+//bigint
+// string =>""
+//boolean => true/false
+//null => standalone value 
+//(khali)
+//undefined =>
+// symbol => unique
+// object
+
+console.log(typeof("vishakha"));

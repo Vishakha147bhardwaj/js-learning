a784e9092b7e728f8a1c228bc5ed29377c6083cb
vishakha bhardwaj <103990579+Vishakha147bhardwaj@users.noreply.github.com>
2026-06-13 08:14:39 +0000

    string basics

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "Vishakha";
+const repoCount = 16;
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('Vish');
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+const newString = gameName.substring(0,2);
+// console.log(newString);
+const anotherString = gameName.slice(-4,2);
+// console.log(anotherString);
+const newStringOne = "    vishakha      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url = "https://vishakha.com/vishakha%20bhardwaj";
+console.log(url.replace('%20','-'));
+console.log(url.includes('sundar'));
+
+const splitArray = 'vishakha-bh-ard-waj';
+console.log(splitArray.split('-'));
+
+
+
+
+
